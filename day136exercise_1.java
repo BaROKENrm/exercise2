9a354eacbccaa0031f080a71d67656ccb69ddac5
BaROKENrm <1336400335@qq.com>
2021-05-11 18:13:51 +0800

    leetcode 1734. 解码异或后的排列

---
+++ day136exercise_1.java
@@ -0,0 +1,21 @@
+package day136;
+//1734. 解码异或后的排列
+public class day136exercise_1 {
+    public int[] decode(int[] encoded) {
+        int n = encoded.length + 1;
+        int total = 0;
+        for (int i = 1; i <= n; i++) {
+            total ^= i;
+        }
+        int odd = 0;
+        for (int i = 1; i < n - 1; i += 2) {
+            odd ^= encoded[i];
+        }
+        int[] perm = new int[n];
+        perm[0] = total ^ odd;
+        for (int i = 0; i < n - 1; i++) {
+            perm[i + 1] = perm[i] ^ encoded[i];
+        }
+        return perm;
+    }
+}
