b608df4e738599115a2da5235a6e10dd3b84b259
BaROKENrm <1336400335@qq.com>
2021-05-28 21:51:14 +0800

    leetcode 447 爆搜

---
+++ day153exercise_2.java
@@ -0,0 +1,15 @@
+package day153;
+//leetcode 447
+public class day153exercise_2 {
+    public int totalHammingDistance(int[] nums) {
+        int ans = 0, n = nums.length;
+        for (int i = 0; i < 30; ++i) {
+            int c = 0;
+            for (int val : nums) {
+                c += (val >> i) & 1;
+            }
+            ans += c * (n - c);
+        }
+        return ans;
+    }
+}
