2446720ea4be71ec0d0941d828cebf20ce609b75
BaROKENrm <1336400335@qq.com>
2021-06-08 20:59:16 +0800

    leetcode 1049. 最后一块石头的重量 II

---
+++ day164exercise_1.java
@@ -0,0 +1,18 @@
+package day164;
+//1049. 最后一块石头的重量 II
+public class day164exercise_1 {
+    public int lastStoneWeightII(int[] ss) {
+        int n = ss.length;
+        int sum = 0;
+        for (int i : ss) sum += i;
+        int t = sum / 2;
+        int[] f = new int[t + 1];
+        for (int i = 1; i <= n; i++) {
+            int x = ss[i - 1];
+            for (int j = t; j >= x; j--) {
+                f[j] = Math.max(f[j], f[j - x] + x);
+            }
+        }
+        return Math.abs(sum - f[t] - f[t]);
+    }
+}
